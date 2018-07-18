3d56f4e641650b7275231425e8f5a859c0d37cff
Emma Rivera <erive011@ucr.edu>
2018-07-17 17:41:07 -0700

    Added People Client and Group creation activity

---
+++ app/src/main/java/com/emmabr/schedulingapp/GroupCreationActivity.java
@@ -0,0 +1,17 @@
+package com.emmabr.schedulingapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.emmabr.schedulingapp.R;
+
+public class GroupCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_creation);
+
+
+    }
+}
