ed0b81262b7d51c18813ed7119bae63c1d110c51
Evan Cornish <ecornish@fb.com>
2018-07-17 18:14:45 -0700

    Final fix attempt of work day 2

--- app/src/main/java/com/emmabr/schedulingapp/LoginActivity.java
+++ app/src/main/java/com/emmabr/schedulingapp/LoginActivity.java
@@ -1,15 +1,140 @@
 package com.emmabr.schedulingapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import me.emmabr.schedulingapp.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // shared Firebase object
+    private FirebaseAuth mAuth;
+
+    Button login;
+    Button register;
+    TextView liEmail;
+    TextView liPassword;
+
+    private ProgressDialog mLoginProgress;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currUser = mAuth.getCurrentUser();
+        updateUI(currUser);
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.btLogIn);
+        register = findViewById(R.id.btRegister);
+        liEmail = findViewById(R.id.tvEmail);
+        liPassword = findViewById(R.id.tvPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mLoginProgress = new ProgressDialog(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = liEmail.getText().toString();
+                final String password = liPassword.getText().toString();
+
+                //login a user
+                if (!TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+                    mLoginProgress.setTitle("Logging In");
+                    mLoginProgress.setMessage("Please wait while we check your credentials.");
+                    mLoginProgress.setCanceledOnTouchOutside(false);
+                    mLoginProgress.show();
+
+                    loginUser(email, password);
+                }
+            }
+        });
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = liEmail.getText().toString();
+                final String password = liPassword.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("login", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    updateUI(user);
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("login", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    updateUI(null);
+                                }
+
+                            }
+                        });
+
+            }
+        });
+    }
+
+    private void updateUI(FirebaseUser userInfo) {
+        if (userInfo != null) {
+            Intent i = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(i);
+        }
+    }
+
+    private void loginUser(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+
+                    mLoginProgress.dismiss();
+
+                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(mainIntent);
+                    finish();
+
+                } else {
+
+                    mLoginProgress.hide();
+                    Toast.makeText(LoginActivity.this, "Cannot Sign in. Please check the form and try again.", Toast.LENGTH_LONG).show();
+
+                }
+
+            }
+        });
+
     }
 }
+
