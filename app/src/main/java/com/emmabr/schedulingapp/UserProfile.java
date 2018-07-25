19b015842d4c84e7fe85f5335ea9875722386edf
Emma Rivera <erive011@ucr.edu>
2018-07-24 18:19:08 -0700

    Added access to internal storage to change image

--- app/src/main/java/com/emmabr/schedulingapp/UserProfile.java
+++ app/src/main/java/com/emmabr/schedulingapp/UserProfile.java
@@ -1,15 +1,160 @@
 package com.emmabr.schedulingapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import me.emmabr.schedulingapp.R;
 
 public class UserProfile extends AppCompatActivity {
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+
+    // Android Layout Variables
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private Button mImageBtn;
+
+    private static final int GALLERY_PICK = 1;
+
+    //Storage Firebase
+    private StorageReference mImageStorage;
+    private ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        mDisplayImage = (CircleImageView) findViewById(R.id.settings_image);
+        mName = (TextView) findViewById(R.id.settingsName);
+        mImageBtn =(Button) findViewById(R.id.settingsImageBtn);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        mImageStorage = FirebaseStorage.getInstance().getReference();
+
+
+        String current_uid = mCurrentUser.getUid();
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(current_uid);
+        mUserDatabase.keepSynced(true);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("nickName").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                //String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+
+                if(!image.equals("default")){
+                    Glide.with(getApplicationContext())
+                            .load(image)
+                            .apply(new RequestOptions()
+                                    .placeholder(R.drawable.default_pic)
+                                    .fitCenter())
+                            .into(mDisplayImage);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+        mImageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CropImage.activity()
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(UserProfile.this);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_PICK && resultCode == RESULT_OK){
+
+            Uri imageUri= data.getData();
+
+            CropImage.activity(imageUri)
+                    .setAspectRatio(1,1)
+                    .start(this);
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+
+                mProgressDialog = new ProgressDialog(UserProfile.this);
+                mProgressDialog.setTitle("Uploading image...");
+                mProgressDialog.setMessage("Please wait while we upload and process the image");
+                mProgressDialog.setCanceledOnTouchOutside(false);
+                mProgressDialog.show();
+
+                Uri resultUri = result.getUri();
+
+                String current_user_id = mCurrentUser.getUid();
+                StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if(task.isSuccessful()){
+                            String download_url = task.getResult().getStorage().getDownloadUrl().toString();
+                            mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()){
+                                        mProgressDialog.dismiss();
+                                        Toast.makeText(UserProfile.this, "Success Uploading", Toast.LENGTH_LONG).show();
+
+
+                                    }
+                                }
+                            });
+                        }else{
+                            Toast.makeText(UserProfile.this, "Error uploading image", Toast.LENGTH_LONG).show();
+                            mProgressDialog.dismiss();
+                        }
+                    }
+                });
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
