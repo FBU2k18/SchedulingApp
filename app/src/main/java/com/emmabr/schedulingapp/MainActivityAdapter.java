cfa3eba2b538ca87d5d691434fef709cdfa57623
Emma Rivera <erive011@ucr.edu>
2018-08-03 16:29:52 -0700

    Added functions for getting users in group and populationg an array

--- app/src/main/java/com/emmabr/schedulingapp/MainActivityAdapter.java
+++ app/src/main/java/com/emmabr/schedulingapp/MainActivityAdapter.java
@@ -15,6 +15,12 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 import com.emmabr.schedulingapp.Models.GroupData;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -26,7 +32,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
 
     ArrayList<GroupData> groups;
     Context context;
-
+    ArrayList<String> groupMembers = new ArrayList<>();
 
     public MainActivityAdapter(ArrayList<GroupData> groups) {
         this.groups = groups;
@@ -35,6 +41,43 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
     @Override
     public void onBindViewHolder(@NonNull MainActivityAdapter.ViewHolder holder, int position) {
         GroupData currGroup = groups.get(position);
+
+        FirebaseDatabase.getInstance().getReference().child("groups").child(currGroup.getGroupId()).child("Recipients").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (final DataSnapshot member : dataSnapshot.getChildren()) {
+                    member.getRef().setPriority(null).continueWithTask(new Continuation<Void, Task<DataSnapshot>>() {
+                        @Override
+                        public Task<DataSnapshot> then(@NonNull Task<Void> task) {
+                            FirebaseDatabase.getInstance().getReference().child("users").child(member.getKey().toString()).child("nickName").addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    groupMembers.add(dataSnapshot.getValue().toString());
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                }
+                            });
+                            return null;
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        String userlist = "";
+        for (int i = 0; i < groupMembers.size() - 1; ++i) {
+            userlist = userlist + groupMembers.get(i) + ", ";
+        }
+        //userlist = userlist + groupMembers.get(groupMembers.size() - 1);
+        holder.tvGroupMembers.setText(userlist);
+
         holder.tvGroupName.setText(currGroup.getGroupName());
         if (currGroup.getImageURL() != null && !currGroup.getImageURL().equals(""))
             Glide.with(context)
@@ -49,6 +92,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
         context = viewGroup.getContext();
         LayoutInflater inflater = LayoutInflater.from(context);
         View groupView = inflater.inflate(R.layout.item_group, viewGroup, false);
+
         return new MainActivityAdapter.ViewHolder(groupView);
     }
 
@@ -61,6 +105,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
 
         ImageView ivGroupLogo;
         TextView tvGroupName;
+        TextView tvGroupMembers;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -68,6 +113,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
 
             ivGroupLogo = itemView.findViewById(R.id.ivGroupLogo);
             tvGroupName = itemView.findViewById(R.id.tvGroupName);
+            tvGroupMembers = itemView.findViewById(R.id.tvGroupMembers);
 
             itemView.setOnClickListener(this);
         }
