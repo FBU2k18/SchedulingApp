64d11b323f10945a1708c45bbaf248c5d99c5a75
Evan Cornish <ecornish@fb.com>
2018-08-07 11:42:34 -0700

    Group Name visible

--- app/src/main/java/com/emmabr/schedulingapp/GroupActivity.java
+++ app/src/main/java/com/emmabr/schedulingapp/GroupActivity.java
@@ -49,6 +49,7 @@ public class GroupActivity extends AppCompatActivity implements LeaveGroupDialog
     private MessageAdapter mMessageAdapter;
     private RecyclerView mRVMessageDisplay;
 
+    private TextView mTVGroupName;
     private EditText mETMessage;
     private FrameLayout mFLPeeker;
     private BottomSheetBehavior mPeekerBehavior;
@@ -83,6 +84,18 @@ public class GroupActivity extends AppCompatActivity implements LeaveGroupDialog
         mRVMessageDisplay.setAdapter(mMessageAdapter);
         mRVMessageDisplay.setLayoutManager(new LinearLayoutManager(this));
 
+        mTVGroupName = findViewById(R.id.tvGroupName);
+        FirebaseDatabase.getInstance().getReference().child("groups").child(mGroupID).child("groupName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                mTVGroupName.setText(dataSnapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         mETMessage = findViewById(R.id.etMessage);
         mFLPeeker = findViewById(R.id.flPeeker);
         mPeekerBehavior = BottomSheetBehavior.from(mFLPeeker);
