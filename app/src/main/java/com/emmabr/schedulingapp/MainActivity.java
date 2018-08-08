13aade3da8d942c5a6e970eec8a0e9b9dce77897
Evan Cornish <ecornish@fb.com>
2018-08-08 10:58:54 -0700

    Minor notification enhancements

--- app/src/main/java/com/emmabr/schedulingapp/MainActivity.java
+++ app/src/main/java/com/emmabr/schedulingapp/MainActivity.java
@@ -187,6 +187,7 @@ public class MainActivity extends AppCompatActivity {
                             pos++;
                         }
                     }
+                    mNotificationManager.cancelAll();
                     mAdapter.notifyDataSetChanged();
                     mSRLMain.setRefreshing(false);
                     mRVGroups.scrollToPosition(0);
