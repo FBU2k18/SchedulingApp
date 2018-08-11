ea49c1dd4111c20ccfd72f3a1b2157be4689bf0f
Krithika Iyer <ksiyer@fb.com>
2018-08-10 17:37:21 -0700

    Minor UI Changes

--- app/src/main/java/com/emmabr/schedulingapp/UserProfile.java
+++ app/src/main/java/com/emmabr/schedulingapp/UserProfile.java
@@ -9,6 +9,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
